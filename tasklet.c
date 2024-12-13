fefcab2d6bdb3839dab2a52c86f77da45ec7ce83
pramodmarella <pramodmarella@mirafra.com>
2024-12-13 11:25:00 +0530

    tasklet_driver.c

--- tasklet.c
+++ tasklet.c
@@ -7,7 +7,7 @@
 #include <linux/ioctl.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("TEAM1");
+MODULE_AUTHOR("Pramod");
 MODULE_DESCRIPTION("Tasklet Example Driver");
 
 #define DEVICE_NAME "tasklet_dev"
