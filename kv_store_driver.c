e3d67162a285aa337500531d948c89d7c8d2036a
Bharat Kambale <bharathkambale@mirafra.com>
2024-12-13 10:52:58 +0530

    kv_store

--- kv_store_driver.c
+++ kv_store_driver.c
@@ -6,9 +6,16 @@
 #define DEVICE_NAME "simple_device"
 #define BUFFER_SIZE 1024
 
+struct kv{
+int key;
+int value;
+};
+
 static int major_number;
 static char device_buffer[BUFFER_SIZE];
 static int open_count = 0;
+static struct kv kv_a[10];
+static int kv_count=0;
 
 // Function prototypes for device operations
 static int device_open(struct inode *inode, struct file *file);
@@ -40,16 +47,30 @@ static int device_close(struct inode *inode, struct file *file) {
 // Called when data is read from the device
 static ssize_t device_read(struct file *file, char __user *user_buffer, size_t size, loff_t *offset) {
     size_t len = strlen(device_buffer);
-
+    int key,i;
     if (*offset >= len) // Check if all data has been read
         return 0;
 
     if (size > len - *offset) // Adjust size to avoid reading beyond buffer
         size = len - *offset;
-
-    if (copy_to_user(user_buffer, device_buffer + *offset, size)) {
-        return -EFAULT;
+	
+    if (copy_from_user(device_buffer, user_buffer, size)) {
+    	return -EFAULT;
     }
+
+    device_buffer[size] = '\0'; // Null-terminate the string
+    
+    key=simple_strtol(device_buffer,NULL,10);
+
+    for(i=0;i<kv_count;i++){
+	if(kv_a[i].key==key){
+		snprintf(device_buffer,BUFFER_SIZE,"%d,%d",kv_a[i].key,kv_a[i].value);
+    		if (copy_to_user(user_buffer, device_buffer, 200)) {
+        		return -EFAULT;
+		}
+	}
+    }
+
 
     *offset += size;
     printk(KERN_INFO "simple_device: Sent %zu bytes to the user\n", size);
@@ -58,14 +79,28 @@ static ssize_t device_read(struct file *file, char __user *user_buffer, size_t s
 
 // Called when data is written to the device
 static ssize_t device_write(struct file *file, const char __user *user_buffer, size_t size, loff_t *offset) {
+    
+    if(kv_count>=10)
+    {
+	printk(KERN_INFO "Buffer full\n");
+	return -EFAULT;
+    }
+
+    int i;
     if (size > BUFFER_SIZE - 1) // Limit size to buffer capacity
-        size = BUFFER_SIZE - 1;
+    size = BUFFER_SIZE - 1;
 
     if (copy_from_user(device_buffer, user_buffer, size)) {
         return -EFAULT;
     }
 
     device_buffer[size] = '\0'; // Null-terminate the string
+    
+    for(i=0;device_buffer[i]!=',';i++);
+    kv_a[kv_count].key=simple_strtol(device_buffer,NULL,10);
+    kv_a[kv_count].value=simple_strtol(device_buffer+i+1,NULL,10);
+    kv_count++;
+
     printk(KERN_INFO "simple_device: Received %zu bytes from the user\n", size);
     return size;
 }
