6268f549de532006603727a976537aaaf9031635
balasai <balasai025@gmail.com>
2024-12-13 14:26:36 +0530

    debugfs

---
+++ Device_drivers/debugfs/.debugfs_driver.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/km/Mirafra_LDD_BATCH/Device_drivers/file_system/debugfs/debugfs_driver.mod := { echo  /home/km/Mirafra_LDD_BATCH/Device_drivers/file_system/debugfs/debugfs_driver.o;  echo; } > /home/km/Mirafra_LDD_BATCH/Device_drivers/file_system/debugfs/debugfs_driver.mod
---
+++ Device_drivers/debugfs/debugfs_driver.mod
@@ -0,0 +1,2 @@
+/home/km/Mirafra_LDD_BATCH/Device_drivers/file_system/debugfs/debugfs_driver.o
+
