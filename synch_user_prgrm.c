28f3262b296c718ba41b7f789c1baccdaf9f825b
NagendraGolla <gollanagendra@mirafra.com>
2024-12-13 11:19:45 +0530

    user_space prgrm for synchronization

---
+++ synch_user_prgrm.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<unistd.h>
+void main()
+{
+	int fd=open("/dev/sync_driver",O_RDONLY);
+	char s[10];
+	read(fd,s,sizeof(s));
+	printf("data=%s\n",s);
+	sleep(30);
+
+}
