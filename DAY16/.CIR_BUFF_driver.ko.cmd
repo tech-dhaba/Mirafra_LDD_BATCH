e113bdbaed23828dbfb28a7c8614020abfb2ef50
vajid-pinjari <vajidmahmadhusen@mirafra.com>
2024-12-21 19:31:57 +0530

    DAY 16

---
+++ DAY16/.CIR_BUFF_driver.ko.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/vajid/Techdhaba/DAY16/CIR_BUFF_driver.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T scripts/module.lds -o /home/vajid/Techdhaba/DAY16/CIR_BUFF_driver.ko /home/vajid/Techdhaba/DAY16/CIR_BUFF_driver.o /home/vajid/Techdhaba/DAY16/CIR_BUFF_driver.mod.o
---
+++ DAY16/.KV_pair.mod.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/vajid/Techdhaba/DAY16/KV_pair.mod := printf '%s\n'   KV_pair.o | awk '!x[$$0]++ { print("/home/vajid/Techdhaba/DAY16/"$$0) }' > /home/vajid/Techdhaba/DAY16/KV_pair.mod
